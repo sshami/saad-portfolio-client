00b7891c9e2fdf26ae15011dc20f0faa13c085c4
Saad Shami <sshami91@gmail.com>
2020-05-07 15:32:14 -0400

    Added tile title animations

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ export default {
     /* Import Vendor Stylings */
     @import '~bootstrap/dist/css/bootstrap.css';
     @import '~socicon/css/socicon.css';
+    @import '~animate.css/animate.css';
 
     /* Colors */
     // TODO: Figure out how to make these global
--- src/static/js/homepage-tiles.js
+++ src/static/js/homepage-tiles.js
@@ -9,15 +9,19 @@ export function initTileHoverTitles() {
 
     /* Show web tile title on hover */
     $("#web-tile").hover(function(){
-        $("#tile-hover-title-web").addClass("show");
+        $("#tile-hover-title-web").removeClass("animate__fadeOutDown");
+        $("#tile-hover-title-web").addClass("animate__animated animate__fadeInUp");
     }, function(){
-        $("#tile-hover-title-web").removeClass("show");
+        $("#tile-hover-title-web").removeClass("animate__fadeInUp");
+        $("#tile-hover-title-web").addClass("animate__animated animate__fadeOutDown");
     });
 
     /* Show photography tile title on hover */
     $("#photography-tile").hover(function(){
-        $("#tile-hover-title-photography").addClass("show");
+        $("#tile-hover-title-photography").removeClass("animate__fadeOutDown");
+        $("#tile-hover-title-photography").addClass("animate__animated animate__fadeInUp");
     }, function(){
-        $("#tile-hover-title-photography").removeClass("show");
+        $("#tile-hover-title-photography").removeClass("animate__fadeInUp");
+        $("#tile-hover-title-photography").addClass("animate__animated animate__fadeOutDown");
     });
 }
