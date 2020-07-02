dcd6020da30ee4e73ea3beea8c373813f5f91f6e
Saad Shami <sshami91@gmail.com>
2020-07-02 13:21:06 -0400

    Added token auth pulled from env variable in axios request header

--- src/common/api-instance.js
+++ src/common/api-instance.js
@@ -5,7 +5,8 @@ export default axios.create({
     baseURL: BACKEND_HOSTNAME + '/api',
     timeout: 5000,
     headers: {
-      'Content-Type' : 'application/json'
+      'Content-Type' : 'application/json',
+      'Authorization': 'Token ' + process.env.VUE_APP_API_TOKEN
     }
 })
   
